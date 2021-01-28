ed7ae8211a13eb4c4d48a17dfc896bb05704a05e
GromZDev <Dmitryev@mail.ru>
2021-01-29 00:33:10 +0300

    Create an empty new project.

---
+++ app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
@@ -0,0 +1,14 @@
+package Android_Intro.Lesson_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
