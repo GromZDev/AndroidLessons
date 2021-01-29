283521280ee646a6df38046db7d44565782ce8f3
GromZDev <Dmitryev@mail.ru>
2021-01-30 01:00:58 +0300

    Added TextView and EditText.
    Added logic in MainActivity.java.
    
    Added two switches and those logic in MainActivity.java
    
    Complete Task no1

--- app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
@@ -8,14 +8,16 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    @SuppressLint({"UseSwitchCompatOrMaterialCode", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +28,14 @@ public class MainActivity extends AppCompatActivity {
         TextView textViewTop = findViewById(R.id.headText);
         Button buttonConfirm = findViewById(R.id.buttonConfirm);
         TextView textViewMiddle = findViewById(R.id.textMiddle);
-
+        TextView checkBoxView = findViewById(R.id.checkBoxText);
 
 
         buttonConfirm.setOnClickListener(new View.OnClickListener() { // Для кнопки
             @Override
             public void onClick(View v) {
                 String userName = String.valueOf(editTextName.getText().toString());
-                textViewMiddle.setText("Glad to see you in my great place, " + userName);
+                textViewMiddle.setText(String.format("Glad to see you in my great place, %s", userName));
             }
         });
 
@@ -44,10 +46,10 @@ public class MainActivity extends AppCompatActivity {
         switchLeft.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     textViewTop.setBackgroundColor(Color.GRAY);
                 } else {
-                    textViewTop.setBackgroundColor(Color.MAGENTA);
+                    textViewTop.setBackgroundColor(Color.argb(100,125,125,125));
                 }
 
             }
@@ -56,18 +58,55 @@ public class MainActivity extends AppCompatActivity {
         switchRight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                String userName = String.valueOf(editTextName.getText().toString());
+                if (isChecked) {
                     buttonConfirm.setText(">>> OK <<<");
                 } else {
                     buttonConfirm.setText("Don't do it!");
                 }
+                checkBoxView.setText("Now choose your side " + userName);
             }
         });
 
-
+        ToggleButton tb = findViewById(R.id.tb);
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                EditText editTextName = findViewById(R.id.editTextName);
+                String userName = String.valueOf(editTextName.getText().toString());
+                TextView bottom = findViewById(R.id.pass);
+                if (isChecked){
+                    bottom.setText("Congratulations " + userName + "! You just completed your Task №1!");
+                } else {
+                    bottom.setText(userName + "! Complete your Task №1!");
+                }
+            }
+        });
 
     }
 
 
+    @SuppressLint({"SetTextI18n", "NonConstantResourceId"})
+    public void onCheckboxClicked(View view) {
+        CheckBox checkBox = (CheckBox) view;
 
+        boolean checked = checkBox.isChecked();
+        EditText editTextName = findViewById(R.id.editTextName);
+        String userName = String.valueOf(editTextName.getText().toString());
+        TextView checkBoxView = findViewById(R.id.jediAnswer);
+
+        // Смотрим, какой именно из флажков отмечен
+        switch (view.getId()) {
+            case R.id.checkBox:
+                if (checked)
+                    checkBoxView.setText("Great " + userName + "! Now you've joined to the Android Light force side!");
+                break;
+            case R.id.checkBox2:
+                if (checked)
+                    checkBoxView.setText("Perfect, " + userName + "! Now you've joined to the Android Dark force side!");
+                break;
+            default:
+                checkBoxView.setText("");
+        }
+    }
 }
