586ab13910577c03f06c759fdea3fed6f789e249
GromZDev <Dmitryev@mail.ru>
2021-01-29 16:54:08 +0300

    Button, Text View and Edit text added.

--- app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
@@ -3,6 +3,9 @@ package Android_Intro.Lesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    EditText editTextName = findViewById(R.id.editTextName); // findViewById - метод, позволяющий
+    // найти элемент по id. R позволяет вытащить его сюда и использовать.
+    TextView textViewTop = findViewById(R.id.headText);
+    Button buttonConfirm = findViewById(R.id.buttonConfirm);
+    TextView textViewMiddle = findViewById(R.id.textMiddle);
+
+
 }
