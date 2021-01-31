bc40b23f05bba89cb55e15b917d1fdcb70b32a55
GromZDev <Dmitryev@mail.ru>
2021-01-31 16:50:47 +0300

    Добавил и расставил кнопки калькулятора. Added and arranged calculator buttons.

--- app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_1/MainActivity.java
@@ -2,7 +2,10 @@ package Android_Intro.Lesson_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button_1);
+
+
+
     }
 }
