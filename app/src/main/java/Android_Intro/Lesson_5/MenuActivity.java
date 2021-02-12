1b8d28aec40e0ef7c7fe517f99cae774c40b89c4
GromZDev <Dmitryev@mail.ru>
2021-02-12 09:22:23 +0300

    Сделал проверку на ввод имени. Если поле пустое, то всплывает окно с предупреждением )

--- app/src/main/java/Android_Intro/Lesson_5/MenuActivity.java
+++ app/src/main/java/Android_Intro/Lesson_5/MenuActivity.java
@@ -1,29 +1,38 @@
 package Android_Intro.Lesson_5;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements DataTransfer {
 
     protected TextView themeDisplay;
     protected String themeName = "Day";
     protected String themeNameD = "Dark";
     protected SaveThemeBySharedPref sharedPreference;
+    protected EditText ed_inputName;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         sharedPreference = new SaveThemeBySharedPref(this); // Берём экземпляр настроек
         if (sharedPreference.loadNightModeState()) {
-            setTheme(R.style.Theme_Lesson_5); // Светлая тема
-        } else setTheme(R.style.DarkTheme_Lesson_5); // Темная тема
+            setTheme(R.style.Theme_Lesson_5);
+        } else {
+            setTheme(R.style.DarkTheme_Lesson_5); // Темная тема
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
@@ -41,6 +50,8 @@ public class MenuActivity extends AppCompatActivity {
 
         changeTheme(); // Логика смены темы
 
+        ed_inputName = findViewById(R.id.inputText); // Edit Text для ввода имени
+
         goToMainActivity(); // Переход в калькулятор
     }
 
@@ -54,15 +65,38 @@ public class MenuActivity extends AppCompatActivity {
     private void goToMainActivity() {
         Button backButton = findViewById(R.id.back_to_calculator);
         backButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MenuActivity.this, MainActivity.class);
-            startActivity(intent);
+            if (ed_inputName.getText().length() != 0) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                intent.putExtra(userName, ed_inputName.getText().toString()); // Захватываем введенное имя
+                intent.putExtra(currentTheme, themeDisplay.getText().toString()); // Захватываем название темы
+                startActivity(intent);
+                finish();
+            } else if (ed_inputName.getText().length() == 0) {
+
+                AlertDialog.Builder alert2 = new AlertDialog.Builder(MenuActivity.this);
+
+                alert2.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //       Toast.makeText(MenuActivity.this, "Now Enter your Name Please", Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                });
+
+                ConstraintLayout alertLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.custom_alert_dialog, null);
+                alert2.setView(alertLayout);
+                alert2.show();
+            }
+
         });
     }
 
     private void changeTheme() { // Смена темы по свичу!
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.DarkTheme_Lesson_5);
-        } else setTheme(R.style.Theme_Lesson_5);
+        } else {
+            setTheme(R.style.Theme_Lesson_5);
+        }
 
         setContentView(R.layout.activity_menu); // Активити сдесь!
 
@@ -81,10 +115,9 @@ public class MenuActivity extends AppCompatActivity {
                 themeDisplay.setText(themeName);
 
             }
-            
+
         });
     }
-
 
     @Override
     protected void onRestoreInstanceState(Bundle saveInstanceState) {
