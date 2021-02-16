7558e6b01c9291de3f79428e2ca44a718852145c
GromZDev <Dmitryev@mail.ru>
2021-02-16 04:07:44 +0300

    Добавил DatePicker реализацию. Пока тестирую.

--- app/src/main/java/Android_Intro/Lesson_6_Notes/DescriptionNote.java
+++ app/src/main/java/Android_Intro/Lesson_6_Notes/DescriptionNote.java
@@ -1,16 +1,23 @@
 package Android_Intro.Lesson_6_Notes;
 
+import android.app.DatePickerDialog;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import Android_Intro.Lesson_6_Notes.NoteFragment;
@@ -18,6 +25,12 @@ import Android_Intro.Lesson_6_Notes.NoteFragment;
 public class DescriptionNote extends Fragment { //TODO 3 Создаем фрагмент остального описания, лэйаут
 
     private TextView textView;
+    private MaterialButton dateButton;
+    String data;
+    String d;
+    private int day;
+    private int month;
+    private int year;
 
 
     public static final String ARGUMENT = "arg_index"; // ключ для передачи описания заметки
@@ -35,12 +48,36 @@ public class DescriptionNote extends Fragment { //TODO 3 Создаем фраг
     //Вытаскиваем лэйаут
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_note_description, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) { // Получаем лэйаут
         super.onViewCreated(view, savedInstanceState);
         textView = view.findViewById(R.id.note_fragment_description);
+
+//================================== Data ==========================
+        dateButton = view.findViewById(R.id.button_date);
+        Calendar calendar = Calendar.getInstance();
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                month = calendar.get(Calendar.MONTH);
+                year = calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        data = SimpleDateFormat.getDateInstance().format(calendar.getTime());
+                        String a = textView.getText().toString().concat(data);
+                        textView.setText(a);
+                    }
+                }, day, month, year);
+                datePickerDialog.show();
+            }
+        });
+
+ // =============================================================
     }
 
     @Override
@@ -53,16 +90,12 @@ public class DescriptionNote extends Fragment { //TODO 3 Создаем фраг
    //         int descId = array.getResourceId(index, 0);
    //         textView.setText(descId);
 
-  //============================
-            String d = NoteFragment.getNoteList().get(index).getNoteDescription().concat(" | ").concat(NoteFragment.getNoteList().get(index).getTheme());
+  //============================ Вывод описания
+            d = NoteFragment.getNoteList().get(index).getNoteDescription().concat(" | ")
+                    .concat(NoteFragment.getNoteList().get(index).getTheme());
                 textView.setText(d);
 
          //   textView.setText(NoteFragment.getNoteList().get(index).getTheme());
-
-
-
-  //============================
-
 
         }
 
