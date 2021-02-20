ce2a3ea3c3458896bec013be1dc2d8be19f370bb
GromZDev <Dmitryev@mail.ru>
2021-02-20 21:38:36 +0300

    Комментарии.

--- app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
@@ -4,33 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static NoteScreen noteScreen = new NoteScreen();
-
-
+    @SuppressLint("StaticFieldLeak")
+    private static final NoteScreen noteScreen = new NoteScreen();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, noteScreen);
         fragmentTransaction.commit();
-
-
-
 
     }
 
     public static NoteScreen getNoteScreen() {
         return noteScreen;
     }
+
 }
