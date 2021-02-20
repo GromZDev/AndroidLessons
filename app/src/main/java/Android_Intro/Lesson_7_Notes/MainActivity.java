974cbaad4d381d02c2181f64c511fa95027aa0e7
GromZDev <Dmitryev@mail.ru>
2021-02-20 15:22:15 +0300

    Создал проект с SingleActivity. В Main сразу показываем первый фрагмент.

--- app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
@@ -1,11 +1,16 @@
 package Android_Intro.Lesson_7_Notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static NoteScreen noteScreen = new NoteScreen();
+
 
 
     @Override
@@ -14,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, noteScreen);
+        fragmentTransaction.commit();
+
+
+
+
+    }
+
+    public static NoteScreen getNoteScreen() {
+        return noteScreen;
     }
 }
