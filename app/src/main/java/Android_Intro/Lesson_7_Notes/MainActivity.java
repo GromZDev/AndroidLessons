b694c569508dac5e57b122afe4207f7ea127fab0
GromZDev <Dmitryev@mail.ru>
2021-02-21 00:25:24 +0300

    Активировал боковое меню в мэйне.

--- app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
@@ -1,14 +1,23 @@
 package Android_Intro.Lesson_7_Notes;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @SuppressLint("StaticFieldLeak")
     private static final NoteScreen noteScreen = new NoteScreen();
@@ -23,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.fragment_container, noteScreen);
         fragmentTransaction.commit();
 
+//====================== Боковое меню ======================
+
+        drawerLayout = findViewById(R.id.drawer_layout); // Из разметки мэйна
+        navigationView = findViewById(R.id.navigation_view); // Из разметки мэйна
+        toolbar = findViewById(R.id.toolbar); // Из разметки мэйна
+//==========================================================
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     public static NoteScreen getNoteScreen() {
