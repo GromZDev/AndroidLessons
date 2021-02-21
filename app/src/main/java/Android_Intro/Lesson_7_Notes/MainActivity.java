522dd3b36b1fcf71c150ed9998d9a08af0c9ffb7
GromZDev <Dmitryev@mail.ru>
2021-02-21 03:26:28 +0300

    Убраз из мэйна тулбар, чтобы у фрагментов он был свой.

--- app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
+++ app/src/main/java/Android_Intro/Lesson_7_Notes/MainActivity.java
@@ -1,5 +1,6 @@
 package Android_Intro.Lesson_7_Notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,6 +10,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -17,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar;
 
     @SuppressLint("StaticFieldLeak")
     private static final NoteScreen noteScreen = new NoteScreen();
@@ -34,14 +37,52 @@ public class MainActivity extends AppCompatActivity {
 
 //====================== Боковое меню ======================
 
-        drawerLayout = findViewById(R.id.drawer_layout); // Из разметки мэйна
-        navigationView = findViewById(R.id.navigation_view); // Из разметки мэйна
-        toolbar = findViewById(R.id.toolbar); // Из разметки мэйна
+//        drawerLayout = findViewById(R.id.drawer_layout); // Из разметки мэйна
+//        navigationView = findViewById(R.id.navigation_view); // Из разметки мэйна
+//        toolbar = findViewById(R.id.toolbar); // Из разметки мэйна
 //==========================================================
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+
+//====================== Ставим тулбар =====================
+     //   initToolbar();
+//====================== ============= =====================
+
+
+
     }
+//    private void initToolbar() {
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//    }
+
+//
+//    @SuppressLint("NonConstantResourceId")
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        int id = item.getItemId();
+//        switch (id){
+//            case R.id.action_settings:
+//                Toast.makeText(MainActivity.this, "Go to Settings", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.action_search:
+//                Toast.makeText(MainActivity.this, "Search MyNote", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.action_sort:
+//                Toast.makeText(MainActivity.this, "Do sort MyNotes", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main_header, menu); // Инфлейтим меню
+//        return true;
+//    }
+
+
 
     public static NoteScreen getNoteScreen() {
         return noteScreen;
