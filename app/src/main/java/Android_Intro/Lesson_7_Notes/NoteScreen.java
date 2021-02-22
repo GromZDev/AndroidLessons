4d00e545b38f9aaabe47018d754580d6ae1c236f
GromZDev <Dmitryev@mail.ru>
2021-02-22 03:35:37 +0300

    Создал новый фрагмент, в который переходим при клике из бокового меню по About App. В классе NoteScreen сделал логику перехода, а также чтобы убиралось боковое меню при возврате.
    Дополнительно сделал так, чтобы при возврате на главный экран не добавлялись автоматически те же заметки, которые инициализируются при включении приложения.

--- app/src/main/java/Android_Intro/Lesson_7_Notes/NoteScreen.java
+++ app/src/main/java/Android_Intro/Lesson_7_Notes/NoteScreen.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
@@ -21,13 +22,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class NoteScreen extends Fragment {
 
-    private static List<MyNote> myNoteArrayList = new ArrayList<>();
+ //   private static List<MyNote> myNoteArrayList = new ArrayList<>(); //
     private TextView tw;
     private NoteDescription fragment = new NoteDescription();
     DrawerLayout drawerLayout;
@@ -50,6 +53,9 @@ public class NoteScreen extends Fragment {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
+        if (clickOnNavigateFragment(id)) {
+            return true;
+        }
         switch (id) {
             case R.id.action_settings:
                 Toast.makeText(getActivity(), "Go to Settings", Toast.LENGTH_SHORT).show();
@@ -71,6 +77,12 @@ public class NoteScreen extends Fragment {
 
         View v = inflater.inflate(R.layout.drawer_fragment_note_screen, container, false);
 
+        initiateSideNavMenu(v); // Наше боковое меню
+
+        return v;
+    }
+
+    private void initiateSideNavMenu(View v) {
         toolbar = toolbarInitiation(v);
 
         drawerLayout = v.findViewById(R.id.drawer_layout);
@@ -79,7 +91,40 @@ public class NoteScreen extends Fragment {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        return v;
+        NavigationView navigationView = v.findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (clickOnNavigateFragment(id)) {
+                    drawerLayout.closeDrawer(GravityCompat.START); // При клике возвращаем меню назад
+                  return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    private boolean clickOnNavigateFragment(int id) {
+        switch (id) {
+            case R.id.side_menu_settings:
+                Toast.makeText(getActivity(), "Going to Settings App", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.side_menu_about:
+                        if (getFragmentManager() != null) {
+                            getFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.fragment_container, new AboutApp())
+                                    //       .addToBackStack(null)
+                                    .commit();
+                            drawerLayout.closeDrawer(GravityCompat.START); // При клике возвращаем меню назад
+                            break;
+                        }
+
+                return true;
+        }
+        return false;
     }
 
     private Toolbar toolbarInitiation(View v) {
@@ -93,10 +138,10 @@ public class NoteScreen extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
-
-        myNoteArrayList.add(new MyNote("Заметка1", "Описание1", "Тема заметки 1"));
-        myNoteArrayList.add(new MyNote("Заметка2", "Описание2", "Тема заметки 2"));
-        myNoteArrayList.add(new MyNote("Заметка3", "Описание3", "Тема заметки 3"));
+        List<MyNote> myNoteArrayList = new ArrayList<>();
+            myNoteArrayList.add(new MyNote("Заметка1", "Описание1", "Тема заметки 1"));
+            myNoteArrayList.add(new MyNote("Заметка2", "Описание2", "Тема заметки 2"));
+            myNoteArrayList.add(new MyNote("Заметка3", "Описание3", "Тема заметки 3"));
 //        myNoteArrayList.add(new MyNote("Заметка4", "Описание4", "Тема заметки 4"));
 //        myNoteArrayList.add(new MyNote("Заметка5", "Описание5", "Тема заметки 5"));
 //        myNoteArrayList.add(new MyNote("Заметка6", "Описание6", "Тема заметки 6"));
@@ -116,7 +161,19 @@ public class NoteScreen extends Fragment {
             tw.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    goToNoteDescriptionWithData(index);
+
+                 //   goToNoteDescriptionWithData(index); // Список заметок будет повторяться при возврате
+                    SettingsStorage ss = new SettingsStorage();
+                    Bundle bundle = new Bundle();
+                    bundle.putString(ss.getDataToFragmentDescription(), myNoteArrayList.get(index).getNoteDescription());
+                    MainActivity.getNoteDescription().setArguments(bundle);
+                    if (getFragmentManager() != null) {
+                        getFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment_container, MainActivity.getNoteDescription())
+                                //           .addToBackStack(null)
+                                .commit();
+                    }
                 }
             });
 
@@ -124,20 +181,18 @@ public class NoteScreen extends Fragment {
         }
     }
 
-    private void goToNoteDescriptionWithData(int index) {
-        //  fragment = new NoteDescription();
-
-        Bundle bundle = new Bundle();
-        bundle.putString("data", myNoteArrayList.get(index).getNoteDescription());
-        MainActivity.getNoteDescription().setArguments(bundle);
-        if (getFragmentManager() != null) {
-            getFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, MainActivity.getNoteDescription())
-                    //           .addToBackStack(null)
-                    .commit();
-        }
-    }
+//    private void goToNoteDescriptionWithData(int index) { // Список заметок будет повторяться при возврате
+//        Bundle bundle = new Bundle();
+//        bundle.putString("data", myNoteArrayList().get(index).getNoteDescription());
+//        MainActivity.getNoteDescription().setArguments(bundle);
+//        if (getFragmentManager() != null) {
+//            getFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.fragment_container, MainActivity.getNoteDescription())
+//                    //           .addToBackStack(null)
+//                    .commit();
+//        }
+//    }
 
 
 }
