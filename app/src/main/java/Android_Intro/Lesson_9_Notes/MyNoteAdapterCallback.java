946cd0b81d2e2211e25e5f78ec70df61c979ffe8
GromZDev <Dmitryev@mail.ru>
2021-02-28 04:57:38 +0300

    В интерфейсе добавил два метода для добавления и сортировки заметок.

--- app/src/main/java/Android_Intro/Lesson_9_Notes/MyNoteAdapterCallback.java
+++ app/src/main/java/Android_Intro/Lesson_9_Notes/MyNoteAdapterCallback.java
@@ -3,4 +3,8 @@ package Android_Intro.Lesson_9_Notes;
 public interface MyNoteAdapterCallback {
 
     void onOnItemClicked(int position);
+
+    void insertNewNote(int position);
+
+    void sortMyNotes();
 }
