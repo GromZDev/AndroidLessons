62c70a3aeb7b5eede0f7a8a16397ab45bb217a7b
GromZDev <Dmitryev@mail.ru>
2021-03-06 22:23:22 +0300

    Подготовил фрагмент добавления заметки. Тут буду сетить базу данных.

--- app/src/main/java/Android_Intro/Lesson_9_Notes/AddNoteFragment.java
+++ app/src/main/java/Android_Intro/Lesson_9_Notes/AddNoteFragment.java
@@ -3,16 +3,26 @@ package Android_Intro.Lesson_9_Notes;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 
 public class AddNoteFragment extends Fragment {
 
     protected View addNoteView;
+    protected EditText editNoteNameText;
+    protected EditText editNoteThemeText;
+    protected EditText editNoteDescText;
+    protected MaterialButton buttonSaveNote;
 
     public static Fragment newInstance(MyNote model) {
         Fragment fragment = new AddNoteFragment();
@@ -35,5 +45,39 @@ public class AddNoteFragment extends Fragment {
 
         addNoteView = inflater.inflate(R.layout.fragment_add_note, container, false);
         return addNoteView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editNoteNameText = view.findViewById(R.id.et_note_name);
+        editNoteThemeText = view.findViewById(R.id.et_note_theme);
+        editNoteDescText = view.findViewById(R.id.et_note_description);
+        buttonSaveNote = view.findViewById(R.id.save_new_note);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        buttonSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = editNoteNameText.getText().toString();
+                final String theme = editNoteThemeText.getText().toString();
+                final String desc = editNoteDescText.getText().toString();
+                saveDataToDB(name, theme, desc);
+            }
+        });
+    }
+
+    private void saveDataToDB(@Nullable String name,
+                              @Nullable String theme,
+                              @Nullable String desc) {
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(theme) && !TextUtils.isEmpty(desc)) {
+            getActivity().onBackPressed();
+        } else {
+            Toast.makeText(requireContext(), "Please input all fields!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
