005536f189fd06098d7c8332bd915fa76567b4df
GromZDev <Dmitryev@mail.ru>
2021-03-06 15:57:34 +0300

    Подключил зависимости для Firebase.

---
+++ app/src/main/java/Android_Intro/Lesson_9_Notes/AddNoteFragment.java
@@ -0,0 +1,29 @@
+package Android_Intro.Lesson_9_Notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddNoteFragment extends Fragment {
+
+    protected View addNoteView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        addNoteView = inflater.inflate(R.layout.fragment_add_note, container, false);
+        return addNoteView;
+    }
+}
