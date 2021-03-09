9960cef88cc1521b7ef2eb0baa8e9b7a4c63d4b8
GromZDev <Dmitryev@mail.ru>
2021-03-09 11:12:02 +0300

    Создал фрагмент и лэйаут к нему для входа через гугл.

---
+++ app/src/main/java/Android_Intro/Lesson_10_Notes/MyNotes/AuthenticationFragment.java
@@ -0,0 +1,38 @@
+package Android_Intro.Lesson_10_Notes.MyNotes;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import Android_Intro.Lesson_10_Notes.R;
+
+public class AuthenticationFragment extends Fragment {
+
+    private View authView;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        authView = inflater.inflate(R.layout.fragment_authentication, container, false);
+        return authView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
